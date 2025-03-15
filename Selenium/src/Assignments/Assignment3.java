4712b61ab8792e0c8742d9f8253128d4074c079d
Gowthaman <Gowthaman@LAPTOP-3R3NJTB7>
2025-03-15 20:52:28 +0530

    hi

--- Selenium/src/Assignments/Assignment3.java
+++ Selenium/src/Assignments/Assignment3.java
@@ -1,11 +1,15 @@
 package Assignments;
 
+import org.testng.Reporter;
+
 public class Assignment3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// click login on demo page-username- javaseleniumA4@gmail.com
-		password-123456
+		//password-123456
+		Reporter.log("Gowtham",true);
+		
 
 	}
 
